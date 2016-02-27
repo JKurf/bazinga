019c29a91103a61762eb86b52fe2ac6d2b256776
Jack <jtkpromo@gmail.com>
2016-02-27 16:48:03 -0500

    Removed HelloWorld

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-
-    System.out.println("Hello World!");
-    System.out.println("Hello darkness my old friend");
-    System.out.println("Simon and Garfunkle");
-  }
-}
