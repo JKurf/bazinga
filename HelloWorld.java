a76815044322b21cd142104f97c4c0f91035b0c6
Jack <jtkpromo@gmail.com>
2016-02-27 16:13:49 -0500

    Look for Test Checker

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("Check for Test");
-    System.out.println("Electric Boogaloo");
+    System.out.println("Changed this Message");
   }
 }
