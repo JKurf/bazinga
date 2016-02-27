bb122111c5755ae2c0dd30c7a9aa26b8f1347f80
Jack <jtkpromo@gmail.com>
2016-02-27 17:58:46 -0500

    Created World Class for Maps

--- World.java
+++ World.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class World {
 
     //int[][] map;
+    //Fuck this
 
     public static void main(String[] args) {
         Scanner fileScan = new Scanner("WorldFiles/TestMap");
