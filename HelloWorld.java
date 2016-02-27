63f016de7e4ca25dc749805cc7920bb3422512b7
Jack <jtkpromo@gmail.com>
2016-02-27 16:32:04 -0500

    Remove other 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println("Check for Test");
-    System.out.println("Changed this Message");
   }
 }
