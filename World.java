5416817d908e2c93f550847bda4e4b169dd852c0
Jack <jtkpromo@gmail.com>
2016-02-27 17:47:18 -0500

    Created World Class for Maps

--- World.java
+++ World.java
@@ -1,9 +1,14 @@
-
+import java.util.Scanner;
 
 public class World {
     int[][] map;
 
     public static void main(String[] args) {
-
+        Scanner fileScan = new Scanner("WorldFiles/TestMap");
+        int c;
+        while (fileScan.hasNextInt()) {
+            c = fileScan.nextInt();
+            System.out.println(c);
+        }
     }
 }
