2ed1dd32f1151e1a0fa60dc471669eead84db456
bitGraf <graf@opmbx.org>
2016-02-28 17:06:05 -0500

    Input Class

---
+++ Inputclass.java
@@ -0,0 +1,37 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class InputClass implements KeyListener {
+    boolean keyStates[];
+
+    public InputClass() {
+        keyStates = new boolean[255];
+        for(int i = 0; i < 255; i++) {
+            keyStates[i] = false;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        /*
+        for(int i = 0; i < 255; i ++) {
+
+            keyStates[i] = (e.getKeyCode() == i);
+            //System.out.print(keyStates[i] + " ");
+        }
+        //System.out.println("");
+        */
+        keyStates[e.getKeyCode()] = true;
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        keyStates[e.getKeyCode()] = false;
+    }
+}
